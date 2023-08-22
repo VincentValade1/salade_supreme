2a8983f55f41a694e7f43e9cbdd3046b426b76dd
VincentValade <vincent.valade@live.fr>
2023-08-22 12:34:48 +0200

    avancée sur le modele projet + page vitrine

--- src/components/ProjetModel.jsx
+++ src/components/ProjetModel.jsx
@@ -1,8 +1,28 @@
 import '../styles/ProjetModel.css'
 
-function ProjetModel ({name}) {
+function ProjetModel ({ bannerImg, altBannerImg, h1, h2, description, img1, alt1, img2, alt2, img3, alt3 }) {
     return(
-        <p className="testProps">{name}</p>
+        // <p className="testProps">{name}</p>
+        <main>
+            <img className='projet-bannerImg' src={bannerImg} alt={altBannerImg}/>
+            <section className="projet-content">
+                <h1 className='pink-titles'>{h1}</h1>
+                <h2>{h2}</h2>
+                <p className='description-projet'>{description}</p>
+                <div className="projet-duo-images-div">
+                    <div className="projet-left-image-div">
+                        <img src={img1} alt={alt1} />
+                        <p>{alt1}</p>
+                    </div>
+                    <div className="projet-right-image-div">
+                        <img src={img2} alt={alt2} />
+                        <p>{alt2}</p>
+                    </div>
+                </div>
+                <img className='projet-lastImg' src={img3} alt={alt3} />
+            </section>
+        </main>
+
     )
 }
 
--- src/pages/SupremeVitrines.jsx
+++ src/pages/SupremeVitrines.jsx
@@ -1,9 +1,17 @@
-import ProjetModel from "../components/ProjetModel"
+import ProjetModel from "../components/ProjetModel";
+import vitrinesData from '../datas/vitrines.json';
 
 function SupremeVitrines() {
     return (
-        <ProjetModel name='vitrines' />
+        <>
+            {vitrinesData.map(( data ) =>
+                (
+                    <ProjetModel bannerImg={data.bannerImg} altBannerImg={data.altBannerImg} h1={data.h1} h2={data.h2} description={data.description} img1={data.img1} alt1={data.alt1} img2={data.img2} alt2={data.alt2} img3={data.img3} alt3={data.alt3} />
+
+                )
+            )}
+        </>
     )
 }
 
-export default SupremeVitrines
+export default SupremeVitrines;
