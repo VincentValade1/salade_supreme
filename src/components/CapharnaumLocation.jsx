1cf5e281a44a30f33ec0bced07708ee30136cbdf
Jeremy Tarrieu <jeremy@tarrieu.fr>
2026-08-25 23:43:29 +0200

    Replace openstreetmap by google maps in capharnaum page

--- src/components/CapharnaumLocation.jsx
+++ src/components/CapharnaumLocation.jsx
@@ -1,32 +1,15 @@
-import { FaMapMarkerAlt, FaRoute } from 'react-icons/fa';
-
 function CapharnaumLocation({ location }) {
-    const mapUrl = `https://www.openstreetmap.org/export/embed.html?bbox=${location.longitude - 0.012}%2C${location.latitude - 0.008}%2C${location.longitude + 0.012}%2C${location.latitude + 0.008}&layer=mapnik&marker=${location.latitude}%2C${location.longitude}`;
-    const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${location.latitude},${location.longitude}`;
-    const directionsUrl = `https://www.google.com/maps/dir/?api=1&destination=${location.latitude},${location.longitude}`;
+    const mapUrl = `https://www.google.com/maps?q=${location.latitude},${location.longitude}&z=17&output=embed`;
 
     return (
         <div className="capharnaum-location-layout">
             <div className="capharnaum-map-wrapper">
                 <iframe
                     className="capharnaum-map"
-                    title="Carte du Capharnaum et des transports proches"
+                    title="Carte Google Maps du Capharnaum et des transports proches"
                     src={mapUrl}
                     loading="lazy"
                 />
-                <a className="capharnaum-map-link" href={location.mapLink} target="_blank" rel="noreferrer noopener">
-                    Ouvrir dans OpenStreetMap
-                </a>
-            </div>
-            <div className="capharnaum-map-actions">
-                <a href={mapsUrl} target="_blank" rel="noreferrer noopener" aria-label="Voir le Capharnaum dans Google Maps">
-                    <FaMapMarkerAlt aria-hidden="true" />
-                    <span>Google Maps</span>
-                </a>
-                <a href={directionsUrl} target="_blank" rel="noreferrer noopener" aria-label="Calculer un itinéraire vers le Capharnaum">
-                    <FaRoute aria-hidden="true" />
-                    <span>Calculer l’itinéraire</span>
-                </a>
             </div>
             <div className="capharnaum-transports">
                 <p className="capharnaum-transports__label">Transports proches</p>
